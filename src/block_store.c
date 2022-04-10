a774d315da6e8ea169bb91e19d75fea9f45783b7
KSU-jsteinbacher17 <69919852+KSU-jsteinbacher17@users.noreply.github.com>
2022-04-10 15:03:22 -0500

    Will finished P3

--- src/block_store.c
+++ src/block_store.c
@@ -9,18 +9,29 @@
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
 
+typedef struct{
+    //unsigned char block[BLOCK_SIZE_BYTES];
+    unsigned char block[BLOCK_SIZE_BYTES];
+}block_t;
 
 //struct block_store block_store_t;
 typedef struct block_store{
+    block_t blocks[BLOCK_STORE_NUM_BLOCKS];
+    bitmap_t *fbm;
+}block_store_t;
+
+/*
+typedef struct block_store{
     char* blocks[BLOCK_STORE_NUM_BLOCKS][BLOCK_SIZE_BYTES];
     bitmap_t *fbm;
-} block_store_t;
+} block_store_t;*/
 
 
 block_store_t *block_store_create()
 {
     // block index where the fbm bitmap starts
     int fbmStartIndex = 127;
+    // use calloc so the memory containing the fbm is initialized to all zeros
     block_store_t *bs = (block_store_t *)calloc(sizeof(block_store_t),1);;
     // number of bits in the bitmap equals the number of blocks
     bs->fbm = bitmap_overlay(BLOCK_STORE_NUM_BLOCKS, &bs->blocks[fbmStartIndex]);
@@ -35,18 +46,13 @@ block_store_t *block_store_create()
     for(int i = fbmStartIndex; i < fbmStartIndex+bitmapNumBlocks; i++){
         block_store_request(bs, i);
     }
-    
-    //bs->fbm = (bitmap_t*)bs->blocks[fbmStartIndex];
-    
-    // block id = block index
-    //block_store_request(bs, fmbStartIndex);
-    
     return bs;
 }
 
 void block_store_destroy(block_store_t *const bs)
 {
     if(bs != NULL){
+        // free the memory used by the block store
         free(bs);
     }
 }
@@ -55,7 +61,9 @@ size_t block_store_allocate(block_store_t *const bs)
     if(bs == NULL){
         return SIZE_MAX;
     }
+    // find the the first zero (unused block) in the fbm
     int id = bitmap_ffz(bs->fbm);
+    // return SIZE_MAX if the end of the file is reached without a free block
     if(id > BLOCK_STORE_AVAIL_BLOCKS){
         return SIZE_MAX;
     }
@@ -69,7 +77,7 @@ bool block_store_request(block_store_t *const bs, const size_t block_id)
     //check for bad parameters, block id is equal to the block index
     if((bs == NULL) || (block_id > BLOCK_STORE_AVAIL_BLOCKS)){
         return false;
-    }   
+    }
     // return false if the requested block is in use
     if(bitmap_test(bs->fbm, block_id)){
         return false;
@@ -98,6 +106,7 @@ size_t block_store_get_used_blocks(const block_store_t *const bs)
 size_t block_store_get_free_blocks(const block_store_t *const bs)
 {
     if(bs != NULL){
+        // calculate the total free blocks
         return (BLOCK_STORE_AVAIL_BLOCKS) - bitmap_total_set(bs->fbm);
     }
     else { return SIZE_MAX; }
@@ -110,8 +119,9 @@ size_t block_store_get_total_blocks()
 
 size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
 {
-    if(bs != NULL && block_id <= BLOCK_STORE_AVAIL_BLOCKS && buffer != NULL){
-        memcpy(buffer, bs->blocks[block_id], BLOCK_SIZE_BYTES);
+    if((bs != NULL) && (block_id < BLOCK_STORE_NUM_BLOCKS) && (buffer != NULL)){
+        // copy the block specified by the block_id to the given buffer
+        memcpy(buffer, &bs->blocks[block_id], BLOCK_SIZE_BYTES);
         return BLOCK_SIZE_BYTES;
     }
     return 0;
@@ -119,8 +129,9 @@ size_t block_store_read(const block_store_t *const bs, const size_t block_id, vo
 
 size_t block_store_write(block_store_t *const bs, const size_t block_id, const void *buffer)
 {
-    if(bs != NULL && block_id <= BLOCK_STORE_AVAIL_BLOCKS && buffer != NULL){
-        memcpy(bs->blocks[block_id], buffer, BLOCK_SIZE_BYTES);
+    if((bs != NULL) && (block_id < BLOCK_STORE_NUM_BLOCKS) && (buffer != NULL)){
+        // write the data from the buffer to the block specified by the block_id
+        memcpy(&bs->blocks[block_id], buffer, BLOCK_SIZE_BYTES);
         return BLOCK_SIZE_BYTES;
     }
     return 0;
@@ -128,13 +139,42 @@ size_t block_store_write(block_store_t *const bs, const size_t block_id, const v
 
 block_store_t *block_store_deserialize(const char *const filename)
 {
-    UNUSED(filename);
-    return NULL;
+    if(filename == NULL){
+        return NULL;
+    }
+    FILE * fp;
+    fp = fopen (filename, "r");
+    // return NULL if error opening file
+    if(fp == NULL){
+        return NULL;
+    }
+    
+    block_store_t * bs = block_store_create();
+    // read BLOCK_STORE_NUM_BLOCKS number of blocks, each of size BLOCK_SIZE_BYTES
+    // from the file to the empty block store array
+    int elementsRead = fread(bs->blocks, BLOCK_SIZE_BYTES, BLOCK_STORE_NUM_BLOCKS, fp);
+    // an error occured if all of the blocks were not read from the file
+    if(elementsRead != BLOCK_STORE_NUM_BLOCKS){
+        printf("Error reading file\n");
+    }
+    return bs;
 }
 
 size_t block_store_serialize(const block_store_t *const bs, const char *const filename)
 {
-    UNUSED(bs);
-    UNUSED(filename);
-    return 0;
+    if(filename == NULL){
+        return 0;
+    }
+    FILE * fp;
+    fp = fopen (filename, "w");
+    // return 0 if error opening file
+    if(fp == NULL){
+        return 0;
+    }
+    // write the enitre block store array the the file
+    int elementsWritten = fwrite(bs->blocks, BLOCK_SIZE_BYTES, BLOCK_STORE_NUM_BLOCKS, fp);
+    
+    // elements written should be the same as BLOCK_STORE_NUM_BLOCKS
+    // returns the total number of bytes written
+    return elementsWritten*BLOCK_SIZE_BYTES;
 }
